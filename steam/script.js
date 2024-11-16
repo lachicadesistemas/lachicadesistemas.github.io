4b74f53ab764be0f857792f7ce5a5416ea478f11
Alexia Steinberg <lachicadesistemas@gmail.com>
2024-11-16 00:40:18 -0300

    x

--- steam/script.js
+++ steam/script.js
@@ -1,3 +1,16 @@
+// Calculadora de precios de juegos en steam
+// Autora: Alexia Michelle <alexia@goldendoglinux.org>
+// Licencia: GNU GPL 3.0
+// Lógica
+
+// Notas adicionales:
+// Esto funciona tomando el valor actual de dolar.txt
+// La logica de esto no está incluída en este software
+// ya que scrappeo un sitio, limpio y paso el valor diario
+// al repositorio.
+// deben buscar la manera de obtener este dato de algun modo u otro
+// y colocarlo en dolar.txt
+
 const calculatorForm = document.getElementById('calculator-form');
 const steamPriceInput = document.getElementById('steam-price');
 const calculateButton = document.getElementById('calculate-button');
